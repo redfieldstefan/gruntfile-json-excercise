9607794b5120b4a2898a9a985511d427776aef72
Stefan Redfield <redfield.stefan@gmail.com>
2015-04-29 10:10:56 -0700

    Add grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-simple-mocha');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.initConfig({
+
+		jshint: {
+			
+			dev:{
+				src:['Gruntfile.js', 'test.js']
+			},
+
+			options: {
+				node: true,
+				globals: {
+					describe: true,
+					it: true,
+					before: true,
+					after: true,
+					beforeEach: true,
+					afterEach: true
+				}
+			}
+		},
+
+		simplemocha: {
+			dev:{
+				src:['test.js']
+			}
+		},
+
+		watch: {
+
+			files:['Gruntfile.js'],
+			tasks:['jshint:dev']
+
+
+		}
+
+	});
+	grunt.registerTask('test', ['jshint:dev', 'simplemocha:dev']);
+	grunt.registerTask('eye', ['watch']);
+	grunt.registerTask('default', ['test']);
+};
